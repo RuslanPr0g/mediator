318e8a268cd688a494878bce24bf0e2b7a4ac597
NB-RUDS\Administrator <ruds@opinov8.com>
2021-06-21 19:30:19 +0300

    Add handlers

---
+++ Mediator/MediatorFromScratch/IHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorFromScratch
+{
+    public interface IHandler<in TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        Task<TResponse> HandleAsync(TRequest request);
+    }
+}
