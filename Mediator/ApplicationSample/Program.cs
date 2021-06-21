b78913052f041e42d3c0c85751b963de8fc514b2
NB-RUDS\Administrator <ruds@opinov8.com>
2021-06-21 19:51:27 +0300

    Add dictionary and send request to mediator

--- Mediator/ApplicationSample/Program.cs
+++ Mediator/ApplicationSample/Program.cs
@@ -1,23 +1,32 @@
 ﻿using MediatorFromScratch;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ApplicationSample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var serviceProvider = new ServiceCollection()
                 .AddTransient<PrintToConsoleHandler>()
                 .BuildServiceProvider();
+
+            var handlerDetails = new Dictionary<Type, Type>()
+            {
+                { typeof(PrintToConsoleRequest), typeof(PrintToConsoleHandler) }
+            };
 
             var request = new PrintToConsoleRequest()
             {
                 Text = "Hello Mediator!"
             };
 
-            IMediator mediator = new Mediator(serviceProvider.GetRequiredService);
+            IMediator mediator = new Mediator(serviceProvider.GetRequiredService, handlerDetails);
+
+            await mediator.SendAsync(request);
 
             // ** The flow:
             // request
