2332d1cbae896e453cbf9d1404d9d32b2bf6473c
NB-RUDS\Administrator <ruds@opinov8.com>
2021-06-21 20:17:22 +0300

    Implement the ServiceCollectionExtentions

--- Mediator/MediatorFromScratch.DependencyInjection/ServiceCollectionExtentions.cs
+++ Mediator/MediatorFromScratch.DependencyInjection/ServiceCollectionExtentions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +16,38 @@ namespace MediatorFromScratch.DependencyInjection
             ServiceLifetime lifetime,
             params Type[] markers)
         {
+            var handler = new Dictionary<Type, Type>();
 
+            foreach (var marker in markers)
+            {
+                var assembly = marker.Assembly;
+                var requests = GetClasses(assembly, typeof(IRequest<>));
+                var handlers = GetClasses(assembly, typeof(IHandler<,>));
+
+                requests.ForEach(x =>
+                {
+                    handler[x] = handlers.SingleOrDefault(y => x == y.GetInterface("IHandler`2")!.GetGenericArguments()[0]);
+                });
+
+                var serviceDescriptor = handlers.Select(x => new ServiceDescriptor(x, x, lifetime));
+                services.TryAdd(serviceDescriptor);
+            }
+
+            services.AddSingleton<IMediator>(x => new Mediator(x.GetRequiredService, handler));
 
             return services;
+        }
+
+        private static List<Type> GetClasses(Assembly assembly, Type typeToMatch)
+        {
+            return assembly.ExportedTypes
+                   .Where(type =>
+                   {
+                       var genericInterfaceTypes = type.GetInterfaces().Where(x => x.IsGenericType).ToList();
+                       var implementRequestType = genericInterfaceTypes
+                           .Any(x => x.GetGenericTypeDefinition() == typeToMatch);
+                       return !type.IsInterface && !type.IsAbstract && implementRequestType;
+                   }).ToList();
         }
     }
 }
