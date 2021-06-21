e511b1eed5449ded43a3a7394f871d00a2ccd362
NB-RUDS\Administrator <ruds@opinov8.com>
2021-06-21 20:02:09 +0300

    add ServiceCollectionExtentions

---
+++ Mediator/MediatorFromScratch.DependencyInjection/ServiceCollectionExtentions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorFromScratch.DependencyInjection
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static IServiceCollection AddMediator(
+            this IServiceCollection services,
+            ServiceLifetime lifetime,
+            params Type[] markers)
+        {
+
+
+            return services;
+        }
+    }
+}
