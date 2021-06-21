5adc7765cbbc10915949caecdc0ad79db59f5d09
NB-RUDS\Administrator <ruds@opinov8.com>
2021-06-21 19:32:51 +0300

    Add handler to application

---
+++ Mediator/ApplicationSample/PrintToConsoleHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatorFromScratch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationSample
+{
+    public class PrintToConsoleHandler : IHandler<PrintToConsoleRequest, bool>
+    {
+        public Task<bool> HandleAsync(PrintToConsoleRequest request)
+        {
+            Console.WriteLine(request.Text);
+            return Task.FromResult(true);
+        }
+    }
+}
