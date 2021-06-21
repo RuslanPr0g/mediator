3e9c56f5798058be465a3a39dff9d563b68c5ad6
NB-RUDS\Administrator <ruds@opinov8.com>
2021-06-21 19:28:45 +0300

    Add requests

---
+++ Mediator/ApplicationSample/PrintToConsoleRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatorFromScratch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationSample
+{
+    public class PrintToConsoleRequest : IRequest<bool>
+    {
+        public string Text { get; init; }
+    }
+}
